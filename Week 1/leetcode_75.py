dd842fbb3ee8d2a6db0cd6f8fca0ddcade069e99
kam1l-p <kpaczkowski33@gmail.com>
2025-03-05 21:47:40 -0500

    Add files via upload

---
+++ Week 1/leetcode_75.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left, right, current = 0, len(nums) - 1, 0
+        
+        while current <= right:
+            if nums[current] == 0:
+                nums[left], nums[current] = nums[current], nums[left]
+                left += 1
+                current += 1
+            elif nums[current] == 2:
+                nums[right], nums[current] = nums[current], nums[right]
+                right -= 1
+            else:
+                current += 1
