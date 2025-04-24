d84f751a65103f21585aaa303e6c5bf3a17913fa
kam1l-p <kpaczkowski33@gmail.com>
2025-04-24 10:47:28 -0400

    Add files via upload

---
+++ Week 7/leetcode_210.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        from collections import deque, defaultdict
+
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
+
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        order = []
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return order if len(order) == numCourses else []
